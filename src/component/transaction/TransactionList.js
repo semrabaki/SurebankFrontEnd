f1f142d5b8add24fdb50e969e401c1d46be37822
Semra Baki <usemrabaki@gmail.com>
2022-04-06 08:37:40 -0500

    updated

--- src/component/transaction/TransactionList.js
+++ src/component/transaction/TransactionList.js
@@ -1,11 +1,74 @@
+import moment from 'moment';
 import React from 'react'
+import { Col, Container, Row, Table } from 'reactstrap';
 
-const TransactionList = () => {
+const TransactionList = (props) => {
+ 
+    let sDate;
+    let eDate;
+
+    if(props.dates){
+        const {startDate,endDate}=props.dates;
+        sDate=moment(startDate).format('MM-DD-YYYY');
+        eDate=moment(endDate).format('MM-DD-YYYY');
+    }
+
   return (
-    <div>
+      <Container>
+          <Row>
+              <Col className="text-center p-3">
+                  <h1>Transactions</h1>
+                  {/* eger transaction varsa table create edior */}
+                    {props.transactions&&props.transactions.length>0?(
+                        <div>
+                           <h4>{sDate +"-"+eDate}</h4>     
+                              
+                              <Table>
+                                  <thead>
+                                      <tr>
+                                          <th>Date</th>
+                                          <th>Type</th>
+                                          <th>Amount</th>
+                                          <th>Balance</th>
+                                          <th>Description</th>
+                                          {/* {props.isAdmin?<th>Account Number</th>:null}
+                                          {props.isAdmin?<th>Name</th>:null} */}
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                  {/* iterating all transactions by using map  and using key for uniqueness and doing conditional styleing*/}
+                                  {
+                                        props.transactions.map((tr,i)=>(
+                  
+                                        <tr key={i} style={
+                                            i%2===0?{backgroundColor:"#EDF1F6"}
+                                            :{backgroundColor:"#AABFBD"}
+                                        }>    
+                                        <td>{moment(tr.date).format("MM-DD-YYYY-hh-mm-ss")}</td>
+                                        <td>{tr.type}</td>
+                                        <td>{tr.amount}</td>
+                                        <td>{tr.availableBalance}</td>
+                                        <td>{tr.description}</td>
+                                        {/* {props.isAdmin&&tr.account?<td>{tr.account.accountNumber}</td>:null}
+                                        {props.isAdmin&&tr.account&&tr.account.user?<td>{tr.account.user.firstName+" "+tr.account.user.lastName}</td>:null} */}
+                                        </tr>
+                                        ))
+                                    }  
 
-        
-    </div>
+                                  </tbody>
+
+                              </Table>
+
+                        </div>
+                    ):(
+                        <p>Transaction List Empty</p>
+                    )}
+
+
+              
+              </Col>
+          </Row>
+      </Container>
   )
 }
 
--- src/component/transaction/TransactionQuery.js
+++ src/component/transaction/TransactionQuery.js
@@ -1,19 +1,36 @@
-import { Field, Formik } from 'formik'
-import React from 'react'
-import TransactionList from './TransactionList'
-import * as Yup from 'yup'
-import { Col, Container, FormFeedback, FormGroup, Input, Label, Row } from 'reactstrap';
+import React from "react";
+import TransactionList from "./TransactionList";
+import * as Yup from "yup";
+import {
+  Button,
+  Col,
+  Container,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+  Spinner,
+} from "reactstrap";
+import { Field, Formik } from "formik";
+import moment from "moment";
+import { toast } from "react-toastify";
+import client from "../../service/SureBankClient";
+import { toastError } from "../../util/Toast";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const TransactionQuerySchema = Yup.object().shape({
-    startDate: Yup.date().required("Please provide a start date"),
-    endDate: Yup.date()
-      .required("Please provide an end date")
-      .min(Yup.ref("startDate"), "end date can't be before start date"),
-  });
+  startDate: Yup.date().required("Please provide a start date"),
+  endDate: Yup.date()
+    .required("Please provide an end date")
+    .min(Yup.ref("startDate"), "end date can't be before start date"),
+});
 
-const TransactionQueryForm=(props)=>(
-    <Container>
-        <fieldset>
+const TransactionQueryForm = (props) => (
+  <Container>
+    <fieldset>
       <legend>Transactions</legend>
       <Row className="justify-content-start">
         <Col lg="2" className="text-center p-3">
@@ -28,45 +45,92 @@ const TransactionQueryForm=(props)=>(
             <FormFeedback>{props.errors.startDate}</FormFeedback>
           </FormGroup>
         </Col>
+        <Col lg="2" className="text-center p-3">
+          <FormGroup>
+            <Label>End Date</Label>
+            <Input
+              tag={Field}
+              name="endDate"
+              type="date"
+              invalid={props.errors.endDate && props.touched.endDate}
+            />
+            <FormFeedback>{props.errors.endDate}</FormFeedback>
+          </FormGroup>
+        </Col>
       </Row>
-      </fieldset>
-    </Container>
-)
 
-const TransactionQuery = () => {
+      <Row className="justify-content-start">
+        <Col lg="4" className="text-center p-3">
+          <Button
+            color="primary"
+            disabled={props.isSubmitting}
+            onClick={props.submitForm}
+          >
+            Search
+          </Button>
+        </Col>
+      </Row>
+      <Row className="justify-content-start">
+          {props.isSubmitting && <Spinner> </Spinner>}
+        </Row>
+    </fieldset>
+  </Container>
+);
+const TransactionQuery = (props) => {
+const [dates,setDates]=useState({});
+const [statement,setStatement]=useState({});
 
-    const submitForm=async(values,actions)=>{
-        try{
-
-            const {startDate,endDate}=values;
 
 
-        }catch(error){
+  const submitForm = async (values, actions) => {
+    try{
+      // we are converting the date the format we want
+      const {startDate,endDate}=values;
+      const d1= moment(startDate,"YYYY-MM-DD");
+      const d2= moment(endDate,"YYYY-MM-DD");
 
-        }
+    // finding the days between to day
+    const diff=  moment.duration(d2.diff(d1)).asDays();
+
+    if(diff>15){
+      toast.error("Between two dates must be max. 15 days", {
+        position:toast.POSITION.TOP_CENTER
+      });
+      actions.setSubmitting(false);
+      return;
     }
 
 
+    setDates({startDate:startDate,endDate:endDate});
+   const response= await client.getCustomerStatement(startDate,endDate);
 
-  return (<>
+   if(response && response.status===200){
+     setStatement(response);
+
+   }
+
+    }catch(error){
+      toastError(error);
+
+    }
+    actions.setSubmitting(false);
+  }
+ return (<>
     <div>
-        <Formik 
-        initialValues={{
-            startDate:"",
-            endDate:""
-        }}
-        validationSchema={TransactionQuerySchema}
-        onSubmit={(values,actions)=>{
-            submitForm(values,actions);
-        }}
-        >
-        component={TransactionQueryForm}
-
-        </Formik>
-    </div>
-    <div><TransactionList/></div>
+        <Formik
+          initialValues={{
+            startDate: "",
+            endDate: "",
+          }}
+          validationSchema={TransactionQuerySchema}
+          onSubmit={(values, actions) => {
+            submitForm(values, actions);
+          }}
+          component={TransactionQueryForm}
+        ></Formik>
+      </div>
+    {/* <div><TransactionList dates={dates} transactions={statement.list}/></div> */}
     </>
-  )
-}
-
+  );
+};
 export default TransactionQuery
